9e8a77dbed440803621c1d5dd6bc8037493d483a
SteffVill <a.villalba.soles@gmail.com>
2026-04-22 22:42:06 -0400

    fix: connect estudianteRoutes in index.js and ensure sync

--- server/routes/estudianteRoutes.js
+++ server/routes/estudianteRoutes.js
@@ -1,11 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const { crearEstudiante, obtenerEstudiantes, obtenerEstudiantePorId, actualizarEstudiante, eliminarEstudiante } = require('../controllers/estudianteController');
-router.post('/', crearEstudiante);
 
+router.post('/', crearEstudiante);
 router.get('/', obtenerEstudiantes); 
 router.get('/:id', obtenerEstudiantePorId);
-
 router.put('/:id', actualizarEstudiante);
 router.delete('/:id', eliminarEstudiante);
 
